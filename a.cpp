95a3ba6f430d0598299de260966063818c23206c
Jihyeun Ryu <water14lily@naver.com>
2024-01-01 17:00:41 +0900

    initialized

---
+++ a.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+cout << 1 << "\n";
+return 0;
+}
+//g++ -std=c++14 -Wall a.cpp -o test.out
+//./test.out
---
+++ a.md
@@ -0,0 +1,2 @@
+g++ -std=c++14 -Wall a.cpp -o result.out
+./result.out
