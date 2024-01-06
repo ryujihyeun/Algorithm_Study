35e04f07dc134f0ed635cb1d9e7272c8cd69917d
Jihyeun Ryu <water14lily@naver.com>
2024-01-06 14:39:16 +0900

    week1_day4

--- 01./N_1629.cpp
+++ 01./N_1629.cpp
@@ -5,6 +5,10 @@ typedef long long ll;
 
 ll go(ll a, ll b){
     if(b == 1) return a % c;
+    ll ret = go(a, b / 2);
+    ret = (ret * ret) % c;
+    if(b % 2) ret = (ret * a) % c;
+    return ret;
 }
 
 int main(){
--- 01./O_4375.cpp
+++ 01./O_4375.cpp
@@ -1,10 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int n;
+
+void go(int n){
+    int cnt = 1, ret = 1;  
+    while(true){
+        if(cnt % n == 0){
+            cout << ret << '\n';
+            break;
+        }
+        else{
+            cnt = (cnt * 10) + 1;
+            cnt %= n;
+            ret++;
+        }
+    }
+}
+
 int main(){
     ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 
-    
+    while(cin >> n){
+        go(n);
+    }
+
     return 0;
 }
